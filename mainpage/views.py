3e9d6ee684353b638d44bc9b2af0f380ec54d0c2
Dmitry Barabash <braboid@yandex.ru>
2020-07-23 21:45:48 +0300

    abc

--- mainpage/views.py
+++ mainpage/views.py
@@ -38,7 +38,7 @@ def main (request):
 	code = request.GET.get('code')
 	url = f"{url_token}?client_id={params['client_id']}&client_secret={params['client_secret']}&redirect_uri={params['redirect_uri']}&code={code}"
 	token_dict = str(requests.get(url).text)
-	token_dict = json.load(requests.get(url=url))
+	token_dict = json.load(requests.get(url).text)
 	access_token = token_dict['access_token']
 	#friends_params['access_token'] = access_token
 	#user_id = token_dict['user_id']
