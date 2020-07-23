e2594f7baee39adc19a8d3632ad47144ef1a227c
Dmitry Barabash <braboid@yandex.ru>
2020-07-23 22:12:13 +0300

    abc

--- mainpage/views.py
+++ mainpage/views.py
@@ -40,9 +40,9 @@ def main (request):
 	#token_dict = str(requests.get(url).text)
 	token_dict = json.loads(requests.get(url).text)
 	access_token = token_dict['access_token']
-	#user_id = token_dict['user_id']
-	#expires_in = token_dict['expires_in']
-	#friends_params['access_token'] = access_token
+	user_id = token_dict['user_id']
+	expires_in = token_dict['expires_in']
+	friends_params['access_token'] = access_token
 	
 	#friends = requests.get(url=url_friends, params=friends_params)
 
@@ -50,4 +50,4 @@ def main (request):
 	#return render(request, 'base.html', {'r2' : r2})
 	#return HttpResponse (r2.text)
 	#return HttpResponse (str(friends.url))
-	return HttpResponse (str(type(token_dict)))
+	return HttpResponse (access_token)
