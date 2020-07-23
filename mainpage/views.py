c71c2185ff9c857b599039a016a96e87d5639f46
Dmitry Barabash <braboid@yandex.ru>
2020-07-23 22:22:03 +0300

    abc

--- mainpage/views.py
+++ mainpage/views.py
@@ -25,7 +25,6 @@ params = {
 friends_params = {
 	'v':'5.52',
 	'access_token': None,
-	'order': 'hints',
 	'count': '5',
 	'fields': 'city, online, photo_100',
 }
@@ -43,10 +42,9 @@ def main (request):
 	user_id = token_dict['user_id']
 	expires_in = token_dict['expires_in']
 	friends_params['access_token'] = access_token
-	friends = json.loads(requests.get(url=url_friends, params=friends_params).text)
+	friends_json = requests.get(url=url_friends, params=friends_params).text
+	friends_dict = json.loads(friends_json)
 
 
 	#return render(request, 'base.html', {'r2' : r2})
-	#return HttpResponse (r2.text)
-	#return HttpResponse (str(friends.url))
-	return HttpResponse (friends)
+	return HttpResponse (friends_json)
