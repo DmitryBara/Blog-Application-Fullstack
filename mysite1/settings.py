e124513172e0e2df7ecb5898397b01f6161063ba
Dmitry Barabash <braboid@yandex.ru>
2020-07-26 00:45:25 +0300

    abc

--- mysite1/settings.py
+++ mysite1/settings.py
@@ -84,6 +84,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+MIDDLEWARE_CLASSES = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    ]
+
 ROOT_URLCONF = 'mysite1.urls'
 
 TEMPLATES = [
@@ -171,3 +177,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
