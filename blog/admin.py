29d4bc91bf1248a11301f1d0a7f7f15e5c3eb982
Dmitry Barabash <braboid@yandex.ru>
2020-08-15 21:33:08 +0300

    .gitignore is now working

--- blog/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from blog.models import Article
-admin.site.register(Article)
--- blog/templatetags/__pycache__/get_field.py
+++
@@ -1,5 +0,0 @@
-from django.template.defaulttags import register
-
-@register.filter
-def get_field(form, field):
-    return 'asdasa'
